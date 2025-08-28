ededaec34fa2ebf8d4136d7c208631675346b781
Oleksandr Kamlykov <aleksandrkamlykov@gmail.com>
2025-08-29 00:16:32 +0300

    fices

--- src/Services/UsersService/DTOs/SalerInfoDto.cs
+++
@@ -1,7 +0,0 @@
-public class SalerInfoResponseDto
-{
-    public Guid Id { get; set; }
-    public string? Description { get; set; }
-    public string? CompanyName { get; set; }
-    public string? Schedule { get; set; }
-}
