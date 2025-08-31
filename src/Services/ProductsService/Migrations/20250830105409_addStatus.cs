58a951e8983291cc20a661bedd31da42fa909eef
Oleksandr Kamlykov <aleksandrkamlykov@gmail.com>
2025-08-31 09:53:53 +0300

    fix

---
+++ src/Services/ProductsService/Migrations/20250830105409_addStatus.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductsService.Migrations
+{
+    /// <inheritdoc />
+    public partial class addStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                schema: "products_service",
+                table: "Feedbacks",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                schema: "products_service",
+                table: "Feedbacks");
+        }
+    }
+}
